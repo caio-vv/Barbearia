310de45584aad7b5ac30d1c7ebc7d551eb38f5fa
Caio <caioviniciusleitelisboa@gmail.com>
2024-08-29 08:26:40 -0300

    feat: pasta de models

--- src/controllers/cliente.js
+++ src/controllers/cliente.js
@@ -1,57 +1,45 @@
-const db = [];
-let nextId = 1;
+const model = require("../models/cliente.js")
 
-const model = (cliente, id = nextId++) => {
-    if (cliente.email != undefined && cliente.email != '' && cliente.telefone != "" && cliente.telefone != undefined && cliente.nome != "" && cliente.nome != undefined) {
-        return {
-            id,
-            email: cliente.email,
-            telefone: cliente.telefone,
-            nome: cliente.nome,
-            senha: cliente.senha
-        };
-    }
-};
+const db = []
 
-const store = body => {
-    const novo = model(body);
+const index = () => db
+
+const show = id => db.find(el => el.id == id)
+
+const store = (body) => {
+    const novo = model(body)
 
     if (novo) {
-        db.push(novo);
-        return 201;
+        db.push(novo)
+        return 201
     }
 
-    return 400;
-};
+    return 400
+}
 
-const index = () => db;
+const update = (body, id) => {
+    const novo = model(body, parseInt(id))
+    const indice = db.findIndex(el => el.id == id)
 
-const show = id => db.find(el => el.id == id);
-
-const update = (id, body) => {
-    const index = db.findIndex(el => el.id == id);
-    const novo = model(body, parseInt(id));
-
-    if (novo && index != -1) {
-        db[index] = novo;
-        return 201;
+    if (novo && indice != -1) {
+        db[indice] = novo
+        return 200
     }
 
-    return 400;
-};
+    return 400
+}
 
 const destroy = id => {
-    const index = db.findIndex(el => el.id == id);
-
-    if (index != -1) {
-        db.splice(index, 1);
+    const indice = db.findIndex(el => el.id == id)
+    if (indice != -1) {
+        db.splice(indice, 1)
     }
-};
+}
 
 module.exports = {
-    store,
     index,
     show,
+    store,
     update,
     destroy
-};
+}

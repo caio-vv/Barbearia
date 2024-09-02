91afcc78c12ae0072c89b4dda38063c82d54fb07
Caio <caioviniciusleitelisboa@gmail.com>
2024-09-02 09:23:51 -0300

    refatorado barbearia

--- src/models/barbearia.js
+++ src/models/barbearia.js
@@ -1,39 +1,24 @@
-let nextId = 1;
 const rede_controller = require("../controllers/rede.js")
 
-const model = (body, id = nextId++) => {
+let proxId = 1;
 
-    let enderecover = true
-    if (body.endereco.cep != undefined &&
-        body.endereco.lograudouro != undefined &&
-        body.endereco.bairro != undefined &&
-        body.endereco.numero != undefined ) {
-            enderecover = true
-        } else {
-            enderecover = false
+const model = (body, id = proxId++) => {
+    if(
+        body.nome != undefined &&
+        body.fotos != undefined &&
+        body.nome != "" &&
+        Array.isArray(body.fotos) &&
+        body.fotos.every(el => el != "") &&
+        rede_controller.show(body.rede_id)
+    ) {
+        return {
+            id,
+            nome: body.nome,
+            fotos: body.fotos,
+            endereco: body.endereco,
+            rede_id: body.rede_id
         }
-        
-  if (
-    body.nome != undefined &&
-    body.fotos != undefined &&
-    body.nome != "" &&
-    body.fotos != "" &&
-    enderecover &&
-    rede_controller.show(body.rede_id)
-  ) {
-    return {
-      nome: body.nome,
-      fotos: body.fotos,
-      endereco: {
-        cep: body.endereco.cep,
-        lograudouro: body.endereco.lograudouro,
-        bairro: body.endereco.bairro,
-        numero: body.endereco.numero,
-        complemento: body.endereco.complemento
-      },
-      rede_id: body.rede_id
-    };
-  }
-};
+    }
+}
 
-module.exports = model;
+module.exports = model
